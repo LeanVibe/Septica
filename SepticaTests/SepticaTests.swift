915f046c27a87547674d9ad689c045cd46dd324a
Bogdan Veliscu <bogdan.veliscu@outlook.com>
2025-09-12 20:23:00 +0300

    Initial Commit

---
+++ SepticaTests/SepticaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SepticaTests.swift
+//  SepticaTests
+//
+//  Created by Bogdan Veliscu on 12.09.2025.
+//
+
+import Testing
+@testable import Septica
+
+struct SepticaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
